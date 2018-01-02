91b4d505e3a23b4a66020bd661b20c21dfd689bc
Adham Gamal <adhammohamadgamal@yahoo.com>
2018-01-02 16:51:19 +0200

    Initial commit

---
+++ Assets/Scripts/AutoMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoMove : MonoBehaviour {
+	private Rigidbody rb;
+	public Text CountText;
+	public Text WinText;
+	private float count;
+	private bool dead;
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		WinText.text = "";
+		dead = false;
+	}
+	void FixedUpdate(){
+		if (!dead) {
+			if (Input.GetKeyDown (KeyCode.Space) && transform.position.y <= 0.5f)
+				rb.AddForce (new Vector3 (0, 1, 0) * 400);
+			transform.position = transform.position + (new Vector3 (0, 0, 1) * Time.deltaTime * 5);
+			count=transform.position.z;
+			setCountText ();
+		}
+	}
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Hurdle"))
+		{
+			WinText.text = "OOOPS.";
+			dead = true;
+		}
+	}
+	void setCountText(){
+		CountText.text = "Count : " + ((int)count).ToString ();
+		if (count >= 490) {
+			WinText.text = "You Win.";
+			dead = true;
+		}
+	}
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	//distance between ball and player
+	private Vector3 distance;
+
+	void Start () {
+		distance = transform.position - player.transform.position;
+	}
+
+	void LateUpdate () {
+		transform.position = distance + player.transform.position;
+	}
+}
